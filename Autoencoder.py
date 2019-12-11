d55b6382845c70270dfb90853abaa8c3bee37796
lordmampf <lordmampf@users.noreply.github.com>
2019-12-11 20:59:02 +0100

    coding improved, print results every 10 epochs

--- Autoencoder.py
+++ Autoencoder.py
@@ -8,13 +8,12 @@ import numpy as np
 import os.path
 from os import path
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 PATH = './sim_autoencoder.pth'
 
-# convert data to torch.FloatTensor
-transform = transforms.ToTensor()
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
 
 # load the training and test datasets
 train_data = datasets.MNIST(root='data', train=True,
@@ -24,16 +23,15 @@ test_data = datasets.MNIST(root='data', train=False,
 								  
 # Create training and test dataloaders
 
-# number of subprocesses to use for data loading
-num_workers = 0
 # how many samples per batch to load
-batch_size = 20
+batch_size = 32
+
+#hidden layer
+encoding_dim = 1
 
 # prepare data loaders
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
-
-
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=0, pin_memory=True, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=0, pin_memory=True, shuffle=True)
 
 # define the NN architecture
 class Autoencoder(nn.Module):
@@ -44,13 +42,13 @@ class Autoencoder(nn.Module):
         self.encoder = nn.Sequential(
             nn.Linear(28 * 28, 256),
             nn.ReLU(True),
-            nn.Linear(256, 64),
+            nn.Linear(256, encoding_dim),
             nn.ReLU(True))
          
         ## decoder ##
         # linear layer (encoding_dim -> input size)
         self.decoder = nn.Sequential(
-            nn.Linear(64, 256),
+            nn.Linear(encoding_dim, 256),
             nn.ReLU(True),
             nn.Linear(256, 28 * 28),
             nn.Sigmoid())
@@ -64,8 +62,7 @@ class Autoencoder(nn.Module):
         return x
 
 # initialize the NN
-encoding_dim = 32
-model = Autoencoder(encoding_dim).cuda()
+model = Autoencoder(encoding_dim)
 print(model)
 
 
@@ -73,7 +70,7 @@ if path.exists(PATH):
     model.load_state_dict(torch.load(PATH))
     model.eval()
 
-model = model.to(device)
+model = model.to("cuda")
 
 # specify loss function
 criterion = nn.BCELoss()
@@ -81,69 +78,91 @@ criterion = nn.BCELoss()
 # specify loss function
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+lossfile = open("loss.txt","w+")
 
-n_epochs = 10
+n_epochloops = 50;
 
-for epoch in range(1, n_epochs+1):
-    # monitor training loss
-    train_loss = 0.0
+for epochLoops in range(0, n_epochloops):
+    n_epochs = 10
     
-    ###################
-    # train the model #
-    ###################
-    for data in train_loader:
-        # _ stands in for labels, here
-        images, _ = data
-        # flatten images
-        images = images.view(images.size(0), -1)
-        # clear the gradients of all optimized variables
-        optimizer.zero_grad()
+    for epoch in range(1, n_epochs+1):
+        # monitor training loss
+        train_loss = 0.0
         
-        images = images.cuda()
-        
-        # forward pass: compute predicted outputs by passing inputs to the model
-        outputs = model(images)
-        # calculate the loss
-        loss = criterion(outputs, images)
-        # backward pass: compute gradient of the loss with respect to model parameters
-        loss.backward()
-        # perform a single optimization step (parameter update)
-        optimizer.step()
-        # update running training loss
-        train_loss += loss.item()*images.size(0)
+        ###################
+        # train the model #
+        ###################
+        for data in train_loader:
+            # _ stands in for labels, here
+            images, _ = data
             
-    # print avg training statistics 
-    train_loss = train_loss/len(train_loader)
-    print('Epoch: {} \tTraining Loss: {:.6f}'.format(
-        epoch, 
-        train_loss
-        ))
-		
-# obtain one batch of test images
-dataiter = iter(test_loader)
-images, labels = dataiter.next()
+            images = images.to("cuda");
+            
+            #flatten images
+            images = images.view(images.size(0), -1)
+            
+            # clear the gradients of all optimized variables
+            optimizer.zero_grad()        
+            
+            # forward pass: compute predicted outputs by passing inputs to the model
+            outputs = model(images)
+                        
+            # calculate the loss
+            loss = criterion(outputs, images)
+            
+            # backward pass: compute gradient of the loss with respect to model parameters
+            loss.backward()
+            
+            # perform a single optimization step (parameter update)
+            optimizer.step()           
+        
+            # update running training loss
+            train_loss += loss.item()*images.size(0)
+                
+        # print avg training statistics 
+        train_loss = train_loss/len(train_loader)
+        
+        realEpoch = epoch + (epochLoops * n_epochs)
+        
+        print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+            realEpoch, 
+            train_loss
+            ))
+        lossfile.write("%d;%f\n" % (realEpoch,train_loss))
+        lossfile.flush()
+        
+    		
+    # obtain one batch of test images
+    dataiter = iter(test_loader)
+    images, labels = dataiter.next()
+    
+    images_flatten = images.view(images.size(0), -1)
+    # get sample outputs
+    output = model(images_flatten.cuda()).cpu()
+    # prep images for display
+    images = images.numpy()
+    
+    # output is resized into a batch of images
+    output = output.view(batch_size, 1, 28, 28)
+    
+    # use detach when it's an output that requires_grad
+    output = output.detach().numpy()
+    
+    # plot the first ten input images and then reconstructed images
+    fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(25,4))
+    
+    # input images on top row, reconstructions on bottom
+    for images, row in zip([images, output], axes):
+        for img, ax in zip(images, row):
+            ax.imshow(np.squeeze(img), cmap='gray')
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+    
+    
+    torch.save(model.state_dict(), PATH)
+    
+    
+print("done!")
+lossfile.close()
 
-images_flatten = images.view(images.size(0), -1)
-# get sample outputs
-output = model(images_flatten.cuda()).cpu()
-# prep images for display
-images = images.numpy()
 
-# output is resized into a batch of images
-output = output.view(batch_size, 1, 28, 28)
-
-# use detach when it's an output that requires_grad
-output = output.detach().numpy()
-
-# plot the first ten input images and then reconstructed images
-fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(25,4))
-
-# input images on top row, reconstructions on bottom
-for images, row in zip([images, output], axes):
-    for img, ax in zip(images, row):
-        ax.imshow(np.squeeze(img), cmap='gray')
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-
-torch.save(model.state_dict(), PATH)
